fbcfc801f3a7e61753861c790d98501b2b8b57fd
hamoudchorfa1997 <86063295+hamoudchorfa1997@users.noreply.github.com>
2021-11-21 22:23:33 +0100

    Add files via upload

---
+++ mini projet javascript SpeedTyper/script.js
@@ -0,0 +1,100 @@
+/*
+mini projet javascript realiser par 
+
+Oumyma Labidi
+Mohamed Chorfa
+Slim Amri
+
+
+*/
+
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settings = document.getElementById("setting");
+const settingsForm = document.getElementById("setting-form");
+const difficultySelect = document.getElementById("difficulty");
+
+const words = [
+  'hello',
+  'tek-up',
+  "javascript",
+  "mini-projet",
+  "success",
+  "determination",
+  "high motivation"];
+
+let randomWord;
+let score = 0;
+let time = 30;
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+difficultySelect.value = difficulty;
+
+text.focus();
+
+const timeInterval = setInterval(updateTime, 1000);
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+addWordToDOM();
+
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+function gameOver() {
+  endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Restart</button>
+    `;
+  endgameEl.style.display = "flex";
+}
+
+text.addEventListener("input", (e) => {
+  const insetedText = e.target.value;
+  if (insetedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+  
+    e.target.value = "";
+    if (difficulty === "hard") {
+      time += 10;
+    } else if (difficulty === "medium") {
+      time += 7;
+    } else {
+      time += 4;
+    }
+
+    updateTime();
+  }
+});
+
+
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
+});
